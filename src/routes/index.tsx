aa388b43389b120890a13c670b33390679fe1bda
Kwanticha20 <puanpeat20@gmail.com>
2024-01-30 16:42:04 +0700

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,9 @@ import {
   BrowserRouter as Router, Routes, Route, Navigate, Outlet
 } from 'react-router-dom';
 import Register from '../view/Register';
+import Login from '../view/Login';
+import Resetpassword from '../view/Resetpassword';
+
 function App() {
   return (
     <Router>
